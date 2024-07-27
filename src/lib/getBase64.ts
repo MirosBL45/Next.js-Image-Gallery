9af173d7ac608cf99c1d64a1586e6549a27ff188
Miroslav Jovic <jovicmiroslav88@gmail.com>
2024-07-27 18:10:08 +0200

    sizes done with lint images

---
+++ src/lib/getBase64.ts
@@ -0,0 +1,19 @@
+import { getPlaiceholder } from 'plaiceholder';
+import type { Photo, ImagesResults } from '@/models/Images';
+
+async function getBase64(imageUrl: string) {
+  try {
+    const res = await fetch(imageUrl);
+
+    if (!res.ok)
+      throw new Error(`Failed to fetch image: ${res.status} ${res.statusText}`);
+
+    const buffer = await res.arrayBuffer();
+
+    const { base64 } = await getPlaiceholder(Buffer.from(buffer));
+
+    return base64;
+  } catch (err) {
+    if (err instanceof Error) console.log(err.stack);
+  }
+}
